e65c0fb3fd5d1806b3dc942b2545392f3697e10f
neginkerim <ngnkrml03@gmail.com>
2025-02-15 15:17:54 +0300

    7

--- js/seventhWeek.js
+++ js/seventhWeek.js
@@ -1,50 +1,25 @@
-// const link = document.querySelector(".link");
-// console.log(link.href); 
-// link.href = "https://www.youtube.com/";
-// console.log(link.href);
-//___________________________________________________________________
-
-const el = document.querySelector(".text")
-const nested = document.querySelector(".sub-text")
-console.log(el.textContent);
-console.log(nested.textContent);
-el.textContent = "Username: Poly";
-console.log(el.textContent);
-
-//__________________________________________________________________
-
-const link = document.querySelector(".link");
-console.log(link.classList); 
-// ["link", "is-active", length: 2, value: "link is-active"]
-
-//____________________________________________________________________
-
-const hasActiveClass = link.classList.contains("is-active");
-const hasActiveTitle = link.classList.contains("title");
-console.log(hasActiveClass);
-console.log(hasActiveTitle);
+// const button = document.querySelector(".btn");
+// const handleClick = (event) => {
+//     console.log("event:", event);
+//     console.log("event type: ", event.type);
+//     console.log("currentTarget: ", event.currentTarget);
+// }
+// button.addEventListener("click", handleClick);
 
 
-const elma = document.querySelector(".elmaSevgisi");
-const elmaVarMi = elma.classList.contains("elmaSevgisi");
-const armutVarMi = elma.classList.contains("armutSevgisi");
-console.log(elmaVarMi);
-console.log(armutVarMi);
 
-//______________________________________________________________________
 
-elma.classList.add("Sevgi");
-console.log(elma.classList);
-
-elma.classList.remove("elmaSevgisi");
-console.log(elma.classList);
-
-//______________________________________________________________________
-
-elma.style.fontSize = "24px";
-elma.style.color = "teal";
-
-//hasAttribute(name): Bu metot, belirtilen özelliğin öğede olup olmadığını kontrol eder. Eğer varsa true, yoksa false döner.
-//getAttribute(name): Bu metot, öğenin belirtilen özelliğinin değerini döndürür. Özellik yoksa null döner.
-//setAttribute(name, value): Bu metot, öğenin belirli bir özelliğine yeni bir değer atar.
-//removeAttribute(name): Bu metot, öğeden belirtilen özelliği kaldırır.
+const addListenerBtn = document.querySelector('.js-add');
+const removeListenerBtn = document.querySelector('.js-remove');
+const btn = document.querySelector(".target-btn");
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+addListenerBtn.addEventListener("click", () => {
+  btn.addEventListener("click", handleClick);
+  console.log("click event listener was added to btn");
+});
+removeListenerBtn.addEventListener("click", () => {
+  btn.removeEventListener("click", handleClick);
+  console.log("click event listener was removed from btn");
+});
