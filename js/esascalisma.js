b2ffdd20aab61229a3a6da0efc2d752836c18db5
neginkerim <ngnkrml03@gmail.com>
2025-03-26 09:02:44 +0300

    oggey

---
+++ js/esascalisma.js
@@ -0,0 +1,7 @@
+/* const btn = document.getElementById('changeColorBtn');
+btn.addEventListener('click', () => {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = `${randomColor}`;
+}); */
+
+//?_______________________________________________________________________________________
